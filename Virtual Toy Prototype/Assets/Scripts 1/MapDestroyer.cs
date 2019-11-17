5a3373b82cea912fd2c9bf2a1ce325bcd0870b3f
Angel Moya <langel_andiyaro@hotmail.com>
2019-11-17 16:24:09 -0500

    create script to explode a bomb

---
+++ Virtual Toy Prototype/Assets/Scripts 1/MapDestroyer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Tilemaps;
+public class MapDestroyer : MonoBehaviour
+{
+    public Tilemap tilemap;
+    public Tile wallTile;
+    public Tile destructibleTile;
+
+    public void Explode(Vector2 worldPos)
+    {
+        Vector3Int originCell= tilemap.WorldToCell(worldPos);
+        ExplodeCell(originCell);
+    }
+
+    void ExplodeCell(Vector3Int cell)
+    {
+        TileBase tile= tilemap.GetTile<Tile>(cell);
+        if (tile == wallTile)
+        {
+            return;
+        }
+        if (tile==destructibleTile)//Remove the tile
+        {
+            tilemap.SetTile(cell, null);
+        }
+        //create a explosion
+    }
+}
